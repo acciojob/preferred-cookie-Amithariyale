c3f4d34478c039637b3fcead8138d775802c20fd
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-25 02:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const form=document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+
+	const d = new Date();
+    d.setTime(d.getTime() + (60 * 2 * 1000));
+    //set a cookie
+    document.cookie = `fontSize=${form.fontsize.value}; expires=${d.toUTCString()}`
+    document.cookie = `fontColor=${form.fontcolor.value}; expires=${d.toUTCString()}`
+	// console.log(document.cookie)
+})
